ddda094e1df6c31c21a1cf162e14e7ab3fed2eb1
ZK <zillekibriya44456@users.noreply.github.com>
2026-06-08 12:05:57 +0530

    Fix Vercel serverless lambda invocation error by using async ES Module handler

--- api/index.js
+++ api/index.js
@@ -1,2 +1,4 @@
-const app = require("../server/index");
-module.exports = app;
+export default async function handler(req, res) {
+  const { default: app } = await import('../server/index.js');
+  return app(req, res);
+}
